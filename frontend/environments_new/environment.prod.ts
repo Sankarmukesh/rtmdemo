94d4bd8fd9c1bad8922ab7ae6ec8b89220381a6b
sankar.yalamandla <sankar.yalamandla@tigeranalytics.com>
2023-01-17 11:23:49 +0000

    new changes

---
+++ frontend/environments_new/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  version: 1,
+  serverUrl: 'https://accelerator-dev.tigeranalytics.com/RTMFastBackend/',
+  liveUrl: 'https://accelerator-dev.tigeranalytics.com/RTMFastBackend/',
+  reportPath: 'https://accelerator-dev.tigeranalytics.com/RTMFastBackend/',
+  defaultLanguage: 'en-US',
+  supportedLanguages: ['en-US', 'fr-FR']
+};  
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   version: 1,
-  serverUrl: 'https://accelerator-dev.tigeranalytics.com/RTMFastBackend/',
-  liveUrl: 'https://accelerator-dev.tigeranalytics.com/RTMFastBackend/',
-  reportPath: 'https://accelerator-dev.tigeranalytics.com/RTMFastBackend/',
+  serverUrl: 'https://fast-tool-dev-be.azurewebsites.net/',
+  liveUrl: 'https://fast-tool-dev-be.azurewebsites.net/',
+  reportPath: 'https://fast-tool-dev-be.azurewebsites.net/',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
-};  
+};
